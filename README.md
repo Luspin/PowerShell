76a346804f418327b2175df34aca9951529b23e6
Luís Pinto <Luspin@microsoft.com>
2022-07-19 21:25:59 +0100

    Added 'Send-SignedCard.ps1'

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-A collection of PowerShell scripts I wrote.
-  
 |File Name																							|Purpose													|
 |---																								|---														|
-|[Get-OAuthToken.ps1](https://github.com/Luspin/PowerShell/blob/main/Scripts/Get-OAuthToken.ps1)	|Retrieve an OAuth token for Microsoft Graph API requests.	|
+|[Get-OAuthToken.ps1](https://github.com/Luspin/PowerShell/blob/main/Scripts/Get-OAuthToken.ps1)	|Retrieves an OAuth token for Microsoft Graph API requests.	|
+|[Send-SignedCard.ps1](https://github.com/Luspin/PowerShell/blob/main/Scripts/Send-SignedCard.ps1)|<sup>*NOT AN ACTUAL SCRIPT!</sup><br>Contains some steps to send [Signed card payloads](https://docs.microsoft.com/en-us/outlook/actionable-messages/security-requirements#signed-card-payloads) over SMTP.	|
   
---
+++ Scripts/Send-SignedCard.ps1
@@ -0,0 +1,35 @@
+# STEP 1: Get the HTML file
+# How to create an open file and folder dialog box with PowerShell
+# https://github.com/myusefulrepo/Tips/blob/master/Tips%20%20-%20How%20to%20create%20an%20open%20file%20and%20folder%20dialog%20box%20with%20PowerShell.md
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog `
+    -Property @{ Filter = 'HTML Source File (*.html)|*.html' }
+
+$null = $FileBrowser.ShowDialog()
+
+$messageBody = Get-Content -Path $FileBrowser.FileNames -Raw
+
+
+# STEP 2: Credentials
+$credential = Get-Credential
+
+
+# STEP 3: Relay the HTML message
+# about Splatting - PowerShell | Microsoft Docs
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_splatting?view=powershell-7.2
+
+$parameters = @{
+    SmtpServer = "smtp.office365.com";
+    Port       = 587;
+    UseSsl     = $true;
+    Credential = $credential;
+    From       = $credential.UserName;
+    To         = "johnDoe@Contoso.com";
+    Subject    = "Signed Actionable Message";
+    Body       = $messageBody;
+    BodyAsHtml = $true;
+}
+
+Send-MailMessage @parameters
